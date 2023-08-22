eceb4886c88801d38e8b0febf460ca7e3281d396
mosesnyota2015 <mosesnyota2015@gmail.com>
2023-08-22 22:00:16 +0300

    Minor updates  _strspn

--- main.h
+++ main.h
@@ -107,7 +107,6 @@ int _strcmp(char *s1, char *s2);
 char *_strchr(char *s, char c);
 int _strspn(char *s, char *accept);
 
-/* aux_mem.c */
 void _memcpy(void *newptr, const void *ptr, unsigned int size);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 char **_reallocdp(char **ptr, unsigned int old_size, unsigned int new_size);
